b1e5729f714e82582bffb91c94b4d85289f57102
driyu <pedriyu@gmail.com>
2017-05-02 21:29:44 +0200

    Implementado la gestión de archivos. Un profesor puede elegir varias asignaturas que imparte y luego subir, de cada asignatura, los apuntes a un directorio específico. El alumno después, si está inscrito en esa asignatura, puede descargar ese contenido. Los archivos se suben con el siguiente código primer par de números el código del profesor, siguientes 2 números codigo de la asignatura. Ejemplo: 2001 archivo.doc 20 sería el codigo del profesor y 01 el código de la asignatura.

--- controllers/archivosController.php
+++ controllers/archivosController.php
@@ -1,19 +1,26 @@
 <?php
 
-class ArchivosController {
+class ArchivosController extends MvcController {
 
         public function archivoNuevoController() {
-       
+            $listadoAsignaturas = ArchivosModel::listarAsignaturasModel($_SESSION["userId"]);
+            
+            //var_dump($listadoAsignaturas);
             echo '<!-- MAX_FILE_SIZE debe preceder al campo de entrada del fichero -->
-            <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
-            <!-- El nombre del elemento de entrada determina el nombre en el array $_FILES -->
-            Enviar este fichero: <input name="fichero_usuario" type="file" multiple/>   
-            <input type="submit" value="Enviar fichero" />';   
+            <input type="hidden" name="MAX_FILE_SIZE" value="300000" />
+            <!-- El nombre del elemento de entrada determina el nombre en el array $_FILES  añadir atributo multiple si quiero subir más de un archivo-->
+            Enviar este fichero: <input name="fichero_usuario" type="file"/> ';
+            echo 'Selecciona la asignatura';
+            foreach ($listadoAsignaturas as $fila => $item) {
+            echo '<input type="radio" name="idAsignatura" value="'.$item["id"].'">'.$item["nombre"].'</input>';
+            }
+            echo '<input type="submit" value="Enviar fichero" />';   
             
             if(isset($_FILES["fichero_usuario"])){
                 $datosController = $_FILES["fichero_usuario"];
-
-            $respuesta = ArchivosModel::archivoNuevoModel($datosController);
+                $idAsignatura= $_POST["idAsignatura"];
+            $asignatura = ($idAsignatura < 10 ) ? '0'.$idAsignatura : $idAsignatura;
+            $respuesta = ArchivosModel::archivoNuevoModel($datosController, $asignatura);
 
 
             //var_dump($respuesta);
@@ -30,9 +37,27 @@ class ArchivosController {
     
     public function listarArchivosController($idAsignatura){
         
-       $respuesta = ArchivosModel::listarArchivosModel($idAsignatura);
+       $respuesta = ArchivosModel::listarArchivosModel();
         
-       print_r($respuesta);
+       $valor =' ';
+       $asignatura = ($idAsignatura < 10 ) ? '0'.$idAsignatura : $idAsignatura;
+       
+       echo $asignatura.'<br>';
+       
+        foreach ($respuesta as $fila => $item) {
+            
+            if($item!='.' && $item!='..' && (substr($item, 2, 2)==$asignatura)){// (substr($item, 0, 2)==$asignatura) si los 2 número de caracteres desde el principio(0) coinciden con la asignatura, se muestran
+                $valor = utf8_encode(substr($item, 4));
+                $direccion ='archivos/'.utf8_encode($item);
+                echo '<div class="contenido-asignatura">';
+                echo "<p><a href='$direccion'>Descargar $valor</a></p>";                
+                echo '</div>';
+                }
+            }
+       
+       
+       
+       
         
     }
     
--- models/archivosModel.php
+++ models/archivosModel.php
@@ -1,25 +1,34 @@
 <?php
 
-class ArchivosModel {
+class ArchivosModel extends Datos {
 
-    public function archivoNuevoModel($datosModel) {     
+    public function archivoNuevoModel($datosModel, $idAsignatura) {     
        
-        if  (move_uploaded_file($datosModel['tmp_name'], 'archivos/' .$_SESSION["userId"]. $datosModel['name'])) {
+       
+        
+        
+        if  (move_uploaded_file($datosModel['tmp_name'], 'archivos/' .$_SESSION["userId"].$idAsignatura.$datosModel['name'])) {// de uno en uno, de momento
            return "ok";
         } else {
             return "ko";
         }
     }
     
-    public function listarArchivosModel($id){
-        
-        
-        
+    public function listarArchivosModel(){
         $directorio =  'archivos';
         $ficheros1  = scandir($directorio);
 
         return $ficheros1;
         
+    }
+    
+    
+    public function listarAsignaturasModel($id){
+        $datosAsignatura = Conexion::conectar()->prepare("SELECT * FROM asignatura WHERE IDprofesor =".$id);
+        $datosAsignatura->execute();
+        
+        return $datosAsignatura->fetchAll();
+        $datosAsignatura->close();
     }
 
 }
