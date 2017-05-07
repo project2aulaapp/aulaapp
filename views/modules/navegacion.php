fbd09fb504cc77a23852fe6a97fa87b2c4bb8b19
Aaron <aaroninvernon96@gmail.com>
2017-05-07 16:21:14 +0200

    Cambios en el menu, realizo este commit antes de modificar el archivo asignaturasController. php

--- views/modules/navegacion.php
+++ views/modules/navegacion.php
@@ -5,38 +5,38 @@ if (isset($_SESSION["validar"])) {
     
     if($_SESSION["rol"]==1){//administrador
         echo '<ul><li><a href="#">Vista de administrador</a></li>
-		<li><a href="index.php?action=usuarios">Autorización de Usuarios</a></li>
-		<li><a href="index.php?action=msg">Enviar mensajes</a></li>		
+		<li><a href="index.php?action=usuarios">Autorizar Usuarios</a></li>
+                <li><a href="index.php?action=lUsuarios">Listar usuarios</a></li>		
 		<li><a href="index.php?action=asig">Añadir asignatura</a></li>	
                 <li><a href="index.php?action=nCurso">Añadir curso</a></li>
                 <li><a href="index.php?action=lCursos">Listar cursos</a></li>
+		<li><a href="index.php?action=msg">Enviar mensajes</a></li>
                 <li><a href="index.php?action=verMensajes">Ver mensajes</a></li>
-                <li><a href="index.php?action=lUsuarios">Listar usuarios</a></li>
-                <li><a href="index.php?action=perfil">Ver tu perfil de usuario</a></li>
-                <li><a href="index.php?action=subirArchivo">Subir archivos al servidor</a></li>
-                <li><a href="index.php?action=calendario">Ver calendario</a></li>
+                <li><a href="index.php?action=subirArchivo">Subir archivos</a></li>
+                <li><a href="index.php?action=perfil">Tu perfil</a></li>
+                <li><a href="index.php?action=calendario">Calendario</a></li>
                 
 	</ul>';
     }else if($_SESSION["rol"]==2){ //profesor
         echo '<ul><li><a href="#">Vista de profesor</a></li>
+              <li><a href="index.php?action=subirArchivo">Subir apuntes</a></li>
+              <li><a href="index.php?action=asignaturasProfesor">Asignaturas profesor</a></li>
+              <li><a href="index.php?action=nPracticas">Subir practicas</a></li>
+              <li><a href="index.php?action=practicas">Ver practicas</a></li>
+              <li><a href="index.php?action=borrarArchivos">Borrar archivos</a></li>
+              <li><a href="index.php?action=pizarra">Iniciar pizarra</a></li>
               <li><a href="index.php?action=msg">Enviar Mensajes</a></li>
               <li><a href="index.php?action=verMensajes">Ver mensajes</a></li>
               <li><a href="index.php?action=perfil">Ver tu perfil de usuario</a></li>
-              <li><a href="index.php?action=subirArchivo">Subir apuntes al servidor</a></li>
-              <li><a href="index.php?action=borrarArchivos">Borrar archivos</a></li>
-              <li><a href="index.php?action=asignaturasProfesor">Asignaturas profesor</a></li>
-              <li><a href="index.php?action=calendario">Ver calendario</a></li>
-              <li><a href="index.php?action=pizarra">Iniciar pizarra</a></li>
-              <li><a href="index.php?action=nPracticas">Subir practicas</a></li>
-              <li><a href="index.php?action=practicas">Ver practicas</a></li>
+              <li><a href="index.php?action=calendario">Calendario</a></li>
               <ul>';
     }else if($_SESSION["rol"]==3){ //alumnos
         echo '<ul><li><a href="#">Vista de alumno</a></li>
+        <li><a href="index.php?action=asignaturas">Ver tus asignaturas</a></li>
         <li><a href="index.php?action=msg">Enviar mensajes</a></li><ul>
         <li><a href="index.php?action=verMensajes">Ver mensajes</a>
-        <li><a href="index.php?action=perfil">Ver tu perfil de usuario</a></li>
-        <li><a href="index.php?action=asignaturas">Ver tus asignaturas</a></li>
-        <li><a href="index.php?action=calendario">Ver calendario</a></li>
+        <li><a href="index.php?action=perfil">Tu perfil</a></li>
+        <li><a href="index.php?action=calendario">Calendario</a></li>
         </ul>';
     }else if($_SESSION["rol"]==4){ //Por autorizar
         echo '<ul><li><a href="#">vista de usuario sin autorizar</a></li><ul>';
