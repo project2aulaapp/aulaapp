ee505f959fcd29f62fc701f9675e2b54e1e8969b
Aaron <aaroninvernon96@gmail.com>
2017-05-19 10:35:39 +0200

    Corregidos un par de fallos con las imagenes y añadido el efecto parallax

--- views/modules/inicio.php
+++ views/modules/inicio.php
@@ -6,38 +6,46 @@
 
 	<p>Duración: 2.000 horas: 2 cursos académicos, incluida la F.C.T.</p>
 
-	<p>Este ciclo se imparte en turno de mañana de 8 a 14.50 con 6 sesiones diarias.</p>
+	<p>Este ciclo se imparte en turno de mañana de 8 a 14.50 con 6 sesiones diarias.
+	Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
 
 	<p><i>Programa tus propias páginas web. Descubre toda la potencialidad de Internet, el lugar donde ahora sedeciden los negocios y las tendencias. Un mundo con posibilidades inagotables.</i></p>
-	<img src="src/img/daw.jpg" />
+	<!--<img src="src/img/daw.jpg" />-->
+	<div class="parallax" id="parallax1"></div>
 
 </article>
-<hr>
+
 <article>
 	<h1>Administración de Sistemas Informáticos en Red</h1>
 
 	<p>Duración: 2.000 horas: 2 cursos académicos, incluida la F.C.T.</p>
 
 	<p>Este ciclo se imparte en turno de mañana de 8 a 14.50 con 6 sesiones diarias. También se imparte en turno de tarde de 15.30 a 21.40 con 6 sesiones diarias.
+		Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
 	</p>
 
 	<p><i>Domina las redes informáticas. Configura servidores web, de mensajería y multimedia. Trabaja con equipos virtualizados. Prepara la defensa de tus sistemas con firewalls y comunicaciones encriptadas. Aprende herramientas de hacking.</i></p>
-	<img src="src/img/asir.jpg" />
+	
+
+	<!--<img src="src/img/asir.jpg" />-->
+	<div class="parallax" id="parallax2"></div>
 
 </article>
-<hr>
+
 <article>
 	<h1>Desarrollo de Aplicaciones Multiplataforma</h1>
 
 	<p>Duración: 2.000 horas: 2 cursos académicos, incluida la F.C.T.</p>
 
 	<p>Este ciclo se imparte en turno de tarde de 15.30 a 21.40 con 6 sesiones diarias.
+		Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
 	</p>
 
 	<p><i>Crea aplicaciones para móviles, tablets o cualquier dispositivo. Desarrolla tus propios programas con los lenguajes más avanzados. Conviertete en un programador experto, la titulación de empleo altamente cualificado más demandada.</i></p>
-	<img src="src/img/dam.jpg" />
+	<!--<img src="src/img/dam.jpg" />-->
+	<div class="parallax" id="parallax3"></div>
 
 </article>
-<hr>
+
 
 </section>
