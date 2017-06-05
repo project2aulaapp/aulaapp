9c9b3e81bd41a6c588601d637d60cf1fa871f8de
driyu <pedriyu@gmail.com>
2017-06-05 20:29:06 +0200

    calendario responsive

--- views/modules/calendario.php
+++ views/modules/calendario.php
@@ -1,3 +1,5 @@
 <section id="calendario">
+    <div class="responsiveCal">
 <iframe src="https://calendar.google.com/calendar/embed?src=gu86uvoofulmj9ajevvbd3m8uk%40group.calendar.google.com&ctz=Europe/Madrid" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    </div>
 </section>
